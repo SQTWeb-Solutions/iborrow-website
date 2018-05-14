0384edb59c4d5a28f5a212fc50b730d8fdd6338a
Quadriphobs1 <abiodunquadriadekunle@gmail.com>
2018-05-14 17:35:01 +0100

    Initiate vue project

---
+++ vue.config.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+  devServer: {
+    open: process.platform === 'darwin',
+    host: '0.0.0.0',
+    port: 9000,
+    https: false,
+    hotOnly: false,
+    overlay: true,
+    useLocalIp: true,
+    proxy: {
+      '/api': {
+        target: 'http://loaclhost:3000',
+        ws: true,
+        pathRewrite: {'^/api': ''},
+        changeOrigin: true
+      }
+    },
+    before: app => {
+      // app is an express instance
+    }
+  },
+  configureWebpack: {
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery',
+        Popper: ['popper.js', 'default']
+      }),
+      new ExtractTextPlugin({ filename: 'common.[chunkhash].css' })
+    ]
+  }
+}
